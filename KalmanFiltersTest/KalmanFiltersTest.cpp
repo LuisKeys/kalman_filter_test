f1c9ab533df40fc15e532e39cb558251cc3d11b6
Luis <luis@accelone.com>
2017-10-16 15:58:13 -0300

    Meassurement update

--- KalmanFiltersTest/KalmanFiltersTest.cpp
+++ KalmanFiltersTest/KalmanFiltersTest.cpp
@@ -1,11 +1,138 @@
-// KalmanFiltersTest.cpp : Defines the entry point for the console application.
-//
+// Write a function 'filter()' that implements a multi -
+// dimensional Kalman Filter for the example given
+//============================================================================
 
 #include "stdafx.h"
+#include <iostream>
+#include "Eigen/Dense"
+#include <vector>
+
+using namespace std;
+using namespace Eigen;
+
+//Kalman Filter variables
+VectorXd xObjectState;	// object state
+MatrixXd PObjectCovariance;	// object covariance matrix
+VectorXd uExternalMotion;	// external motion
+MatrixXd FStatetransition; // state transition matrix
+MatrixXd HMeassurement;	// measurement matrix
+MatrixXd RMeassurementCovariance;	// measurement covariance matrix
+MatrixXd Identity; // Identity matrix
+MatrixXd QProcessCovariance;	// process covariance matrix
+
+vector<VectorXd> measurements;
+void filter(VectorXd &x, MatrixXd &P);
+
+	int main() {
+		/**
+		* Code used as example to work with Eigen matrices
+		*/
+		//	//you can create a  vertical vector of two elements with a command like this
+		//	VectorXd my_vector(2);
+		//	//you can use the so called comma initializer to set all the coefficients to some values
+		//	my_vector << 10, 20;
+		//
+		//
+		//	//and you can use the cout command to print out the vector
+		//	cout << my_vector << endl;
+		//
+		//
+		//	//the matrices can be created in the same way.
+		//	//For example, This is an initialization of a 2 by 2 matrix
+		//	//with the values 1, 2, 3, and 4
+		//	MatrixXd my_matrix(2,2);
+		//	my_matrix << 1, 2,
+		//			3, 4;
+		//	cout << my_matrix << endl;
+		//
+		//
+		//	//you can use the same comma initializer or you can set each matrix value explicitly
+		//	// For example that's how we can change the matrix elements in the second row
+		//	my_matrix(1,0) = 11;    //second row, first column
+		//	my_matrix(1,1) = 12;    //second row, second column
+		//	cout << my_matrix << endl;
+		//
+		//
+		//	//Also, you can compute the transpose of a matrix with the following command
+		//	MatrixXd my_matrix_t = my_matrix.transpose();
+		//	cout << my_matrix_t << endl;
+		//
+		//
+		//	//And here is how you can get the matrix inverse
+		//	MatrixXd my_matrix_i = my_matrix.inverse();
+		//	cout << my_matrix_i << endl;
+		//
+		//
+		//	//For multiplying the matrix m with the vector b you can write this in one line as let’s say matrix c equals m times v.
+		//	//
+		//	MatrixXd another_matrix;
+		//	another_matrix = my_matrix*my_vector;
+		//	cout << another_matrix << endl;
 
 
-int main()
-{
-    return 0;
-}
+		//design the KF with 1D motion
+		xObjectState = VectorXd(2);
+		xObjectState << 0, 0;
 
+		PObjectCovariance = MatrixXd(2, 2);
+		PObjectCovariance << 1000, 0, 0, 1000;
+
+		uExternalMotion = VectorXd(2);
+		uExternalMotion << 0, 0;
+
+		FStatetransition = MatrixXd(2, 2);
+		FStatetransition << 1, 1, 0, 1;
+
+		HMeassurement = MatrixXd(1, 2);
+		HMeassurement << 1, 0;
+
+		RMeassurementCovariance = MatrixXd(1, 1);
+		RMeassurementCovariance << 1;
+
+		Identity = MatrixXd::Identity(2, 2);
+
+		QProcessCovariance = MatrixXd(2, 2);
+		QProcessCovariance << 0, 0, 0, 0;
+
+		//create a list of measurements
+		VectorXd single_meas(1);
+		single_meas << 1;
+		measurements.push_back(single_meas);
+		single_meas << 2;
+		measurements.push_back(single_meas);
+		single_meas << 3;
+		measurements.push_back(single_meas);
+
+		//call Kalman filter algorithm
+		filter(xObjectState, PObjectCovariance);
+
+		return 0;
+	}
+
+
+	void filter(VectorXd &x, MatrixXd &PObjectCovariance) {
+
+		for (unsigned int n = 0; n < measurements.size(); ++n) {
+
+			VectorXd zMeassurement = measurements[n];
+			//YOUR CODE HERE
+
+			// KF Measurement update step
+			VectorXd yError = zMeassurement - HMeassurement * x;
+			MatrixXd Ht = HMeassurement.transpose();
+			MatrixXd S = HMeassurement * PObjectCovariance * Ht + RMeassurementCovariance;
+			MatrixXd Si = S.inverse();
+			MatrixXd K = PObjectCovariance * Ht * Si;
+
+			// new state
+			x = x + (K * yError);
+			PObjectCovariance = (Identity - K * HMeassurement) * PObjectCovariance;
+
+			// KF Prediction step
+
+			std::cout << "x=" << std::endl << x << std::endl;
+			std::cout << "P=" << std::endl << PObjectCovariance << std::endl;
+
+
+		}
+	}
